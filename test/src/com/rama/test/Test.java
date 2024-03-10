41f3fce48b1cc3461f7417b3e3932038fe6f7c71
Dev1 <Dev1@gmail.com>
2024-03-10 10:06:13 +0530

    Created Ojbect for Utility class

--- test/src/com/rama/test/Test.java
+++ test/src/com/rama/test/Test.java
@@ -1,7 +1,11 @@
 package com.rama.test;
 
+import com.rama.beans.Utility;
+
 public class Test {
 	public static void main(String args[]) {
+		Utility ul=new Utility();
+		System.out.println(ul.getSysDate());
 		
 	}
 
